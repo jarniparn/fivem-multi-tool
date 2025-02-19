954421356e5c63393842f89b64ca07f0443d8b28
jarniparn <jarno0527@gmail.com>
2025-02-19 14:40:23 +0100

    Add files via upload

--- main.bat
+++ main.bat
@@ -2,40 +2,42 @@
 title Multitool - by Jxoni
 chcp 65001 >nul
 cd files
-color 4
 :start
+color 0c
 call :banner
 
-:menu 
-for /f %%A in ('"prompt $H &echo on &for %%B in(1) do rem"') do set BS=%%A
-echo.
-echo.
-echo             ╔═(1) Process Hacker 2
-echo             ║
-echo             ║══(2) dll cheats
-echo             ║
-echo             ║═══(3) x48
-echo             ║
-echo             ╚════(4) x48 v1 
-echo             ║
-echo             ╚═════(5)Fivem-External.exe
-echo              ║
-set /p input=.%bs%            ╚══════^>
+:menu
+for /F %%A in ('"prompt $H & echo on & for %%B in (1) do rem"') do set BS=%%A
+echo. 
+echo. 
+echo [38;2;255;255;0m             ╔═(1) Process Hacker 2
+echo [38;2;255;255;0m             ║
+echo [38;2;255;255;0m             ║══(2) dll cheats
+echo [38;2;255;255;0m             ║
+echo [38;2;255;255;0m             ║═══(3) x48
+echo [38;2;255;255;0m             ║
+echo [38;2;255;255;0m             ╚════(4) x48 v1 
+echo [38;2;255;255;0m             ║
+echo [38;2;255;255;0m             ╚═════(5)Fivem-External.exe
+echo [38;2;255;255;0m              ║              
+set /p input= %bs%              ╚══════^>
 if /I %Input% EQU 1 STARt ph2.lnk
 IF /I %Input% EQU 2 (START explorer "dll cheats")
 if /I %Input% EQU 3 start x48.exe
 if /I %Input% EQU 4 START x48v1.exe
-if /I %Input% EQU 5 START Fivem-External.exe
+if /I %Input% EQU 5 START fivem2.exe
 cls
 goto start
 
 :banner
 echo.
 echo.
-echo                     ███╗   ███╗██╗   ██╗██╗  ████████╗██╗    ████████╗ ██████╗  ██████╗ ██╗     
-echo                     ████╗ ████║██║   ██║██║  ╚══██╔══╝██║    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     
-echo                     ██╔████╔██║██║   ██║██║     ██║   ██║       ██║   ██║   ██║██║   ██║██║     
-echo                     ██║╚██╔╝██║██║   ██║██║     ██║   ██║       ██║   ██║   ██║██║   ██║██║     
-echo                     ██║ ╚═╝ ██║╚██████╔╝███████╗██║   ██║       ██║  ╚██████╔╝╚██████╔╝███████╗
-echo                     ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝       ╚═╝   ╚═════╝  ╚═════╝ ╚══════╝
+echo                         [38;2;255;0;0m███╗   ███╗██╗   ██╗██╗  ████████╗██╗    ████████╗ ██████╗  ██████╗ ██╗[0m
+echo                         [38;2;255;51;0m████╗ ████║██║   ██║██║  ╚══██╔══╝██║    ╚══██╔══╝██╔═══██╗██╔═══██╗██║[0m
+echo                         [38;2;255;102;0m██╔████╔██║██║   ██║██║     ██║   ██║       ██║   ██║   ██║██║   ██║██║[0m
+echo                         [38;2;255;153;0m██║╚██╔╝██║██║   ██║██║     ██║   ██║       ██║   ██║   ██║██║   ██║██║[0m
+echo                         [38;2;255;204;0m██║ ╚═╝ ██║╚██████╔╝███████╗██║   ██║       ██║   ╚██████╔╝╚██████╔╝███████╗[0m
+echo                         [38;2;255;255;0m╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝[0m
 echo.
+echo                                           [38;2;255;255;0m        DISCORD.GG/6SQAS8V4
+echo.                                                                                                                                                                                                                                                                                                               
